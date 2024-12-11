86b834565e4b0ffb233d58b22971b47a8767574c
TUANLTPH30142 <letientuan0107@gmail.com>
2024-12-11 15:24:44 +0700

    no message

--- Du_An_TTS_Test/src/main/java/com/example/Du_An_TTS_Test/Repository/RoleRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.Du_An_TTS_Test.Repository;
-
-import com.example.Du_An_TTS_Test.Entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepo extends JpaRepository<Role,String> {
-}
